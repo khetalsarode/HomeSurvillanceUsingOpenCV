be2e08ebb3a53dd14634eeabbed37f83062be5d6
khetalsarode <35837530+khetalsarode@users.noreply.github.com>
2020-02-07 22:01:58 +0530

    Update call.py

--- call.py
+++ call.py
@@ -1,5 +1,4 @@
 from twilio.rest import Client
-import tkinter as tk
 
 def write():
     account_sid = "Enter account sid from twilio account"
@@ -11,20 +10,3 @@ def write():
         from_="enter the twilio generated number",
         url="https://drive.google.com/drive/my-drive/samplw.xml"
     )
-
-    print(call.sid)
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
-
-button = tk.Button(frame, 
-                   text="QUIT", 
-                   fg="red",
-                   command=quit)
-button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Hello",
-                   command=write)
-slogan.pack(side=tk.LEFT)
-
-root.mainloop()
